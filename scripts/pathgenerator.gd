8a940ed25854154dc7052f91eff9b95dd6e54d84
JoshuaxLasley <jlasley@ftstudent.org>
2024-09-18 13:12:17 -0400

    Day 2

--- scripts/pathgenerator.gd
+++ scripts/pathgenerator.gd
@@ -19,6 +19,21 @@ func generate_path():
 	var x = 0
 	var y = int(_grid_height/2)
 
-	var choice:int = randi_range(0,2)
+
+	while x < _grid_length:
+		if not _path.has(Vector2i(x,y)):
+			_path.append(Vector2i(x,y))
+
+		var choice:int = randi_range(0,2)
+
+		if choice == 0 or x % 2 == 0:
+			x += 1
+		elif choice == 1 and y < _grid_height:
+			y += 1
+		elif choice == 2 and y > 0:
+			y -= 1
+
+	return _path
+
 
 
