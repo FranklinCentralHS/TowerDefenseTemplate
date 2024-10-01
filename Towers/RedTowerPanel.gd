4a0c89fe235efb64a1ecb624481679e43d5504cf
fchsLME <lestes0@ftstudent.org>
2024-10-01 13:51:17 -0400

    Update README and Panel

--- Towers/RedTowerPanel.gd
+++ Towers/RedTowerPanel.gd
@@ -1,11 +1,9 @@
 extends Panel
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var tower = preload("res://Towers/RedBullet.tscn")
+var currTile
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_gui_input(event):
+	var tempTower = tower.instantiate()
+	print(event)
